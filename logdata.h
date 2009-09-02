995a3cb38bc0babd82a6c744919b6d2bb206a376
Nicolas Bonnefon <nicko@localhost.(none)>
2009-09-02 23:06:02 +0100

    Created the LogFilteredData class and associated unit test.

--- logdata.h
+++ logdata.h
@@ -4,11 +4,14 @@
 #include <QByteArray>
 #include <QString>
 
+#include "logfiltereddata.h"
+
 class LogData {
     public:
         LogData(const QByteArray &byteArray);
         QString getLineString(int line);
         int getNbLine();
+        LogFilteredData getNewFilteredView(QRegExp& regExp);
 
     private:
         QByteArray* data;
--- tests/logdata_test.cpp
+++ tests/logdata_test.cpp
@@ -1,27 +1,35 @@
 #include "logdata_test.h"
 #include "logdata.h"
 
-static const int NB_LINES = 999999;
+#if QT_VERSION < 0x040500
+#define QBENCHMARK
+#endif
+
+static const int NB_LINES = 9999;
 
 void TestLogData::simpleLoad()
 {
     char* line="LOGDATA is a part of LogCrawler, we are going to test it thoroughly, this is line 000000\n";
 
+    LogData* logData;
     QByteArray array = QByteArray(line);
 
     for (int i = 1; i < NB_LINES; i++)
         array += line;
 
-    LogData* logData = new LogData(array);
+    QBENCHMARK {
+        logData = new LogData(array);
+    }
     QVERIFY(logData != NULL);
     QVERIFY(logData->getNbLine() == NB_LINES);
 
     // Read all lines sequentially
     QString s;
-    for (int i = 1; i < NB_LINES; i++)
-        s = logData->getLineString(i);
-    QVERIFY(s == line);
-
+    QBENCHMARK {
+        for (int i = 0; i < NB_LINES; i++)
+            s = logData->getLineString(i);
+    }
+    QVERIFY(s == QString(line));
 
     delete logData;
 }

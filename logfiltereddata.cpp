68e690b9ba6c8cf96f58c2f5b9ea920033488a38
Nicolas Bonnefon <nicolas@bonnefon.org>
2009-09-11 18:16:36 +0100

    Implement basic filtering with regex.

--- logfiltereddata.cpp
+++ logfiltereddata.cpp
@@ -1,19 +1,32 @@
+#include "log.h"
+
 #include <QString>
 
 #include "logfiltereddata.h"
 
 LogFilteredData::LogFilteredData(QByteArray* logData, QRegExp regExp) : AbstractLogData()
 {
+    LOG(logDEBUG) << "Entering LogFilteredData constructor";
+
+    int pos = 0, end = 0;
+    while ( (end = logData->indexOf("\n", pos)) != -1 ) {
+        const QString string = QString(logData->mid(pos, end-pos));
+        pos = end+1;
+        if ( regExp.indexIn(string) != -1 )
+            lineList.append(string);
+    }
+
+    LOG(logDEBUG) << "End LogFilteredData";
 }
 
 QString LogFilteredData::doGetLineString(int line) const
 {
-    QString string(line);
+    QString string = lineList[line];
 
     return string;
 }
 
 int LogFilteredData::doGetNbLine() const
 {
-    return 4;
+    return lineList.length();
 }

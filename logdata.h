e08ea43b90375a4f7ec79e79c83b2cb816bc3e56
Nicolas Bonnefon <nicolas@bonnefon.org>
2009-09-22 20:13:49 +0100

    Add the GPL notice to all the files.

--- logdata.h
+++ logdata.h
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2009 Nicolas Bonnefon and other contributors
+ *
+ * This file is part of LogCrawler.
+ *
+ * LogCrawler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * LogCrawler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with LogCrawler.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef LOGDATA_H
 #define LOGDATA_H
 

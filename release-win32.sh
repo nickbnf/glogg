9eb5d14d69e5bf738fef4b48e79ae66c77b1c144
Nicolas Bonnefon <nicolas@bonnefon.org>
2009-11-16 13:15:05 +0000

    Update the Windows icon.

--- release-win32.sh
+++ release-win32.sh
@@ -6,10 +6,5 @@ mingw32-make
 cp /cygdrive/c/qt/2009.03/qt/bin/{QtCore4,QtGui4}.dll release/
 cp /cygdrive/c/qt/2009.03/mingw/bin/{libexpat-1,mingwm10}.dll release/
 VERSION=`git describe`
-echo Generating binary distribution for glogg-$VERSION
-rm glogg-$VERSION-win32.zip
-cd release
-zip -9 ../glogg-$VERSION-win32.zip *.dll *.exe
-cd ..
 echo Generating installer for glogg-$VERSION
 /cygdrive/c/Program\ Files/NSIS/makensis -DVERSION=$VERSION glogg.nsi

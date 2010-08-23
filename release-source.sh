68160a587e826f612818f316800c538e5eacab1b
Nicolas Bonnefon <nicolas@bonnefon.org>
2010-08-23 16:51:28 +0100

    Use the right timestamp when generating the source tarball.

--- release-source.sh
+++ release-source.sh
@@ -2,6 +2,7 @@
 
 VERSION=$(git describe | sed -e "s/^v//")
 echo "echo \"$VERSION\" > .tarball-version"
+echo 'touch --date="$(git log -n 1 --pretty=format:%ci)" .tarball-version'
 echo "git archive --format=tar --prefix=glogg-$VERSION/ v$VERSION >glogg-$VERSION.tmp"
 echo "tar --append -f glogg-$VERSION.tmp --transform s_^_glogg-$VERSION/_ .tarball-version"
 echo "gzip < glogg-$VERSION.tmp > glogg-$VERSION.tar.gz"

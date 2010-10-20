6a9e5a533ceced00013ce708d721e7ddff48be85
Nicolas Bonnefon <nicolas@bonnefon.org>
2010-10-20 21:55:40 +0100

    Adjust the performance analysis script.

--- tools/perfmeter.pl
+++ tools/perfmeter.pl
@@ -9,13 +9,13 @@
 
 while (<>) {
     strip;
-    if (/(\d\d\.\d\d\d) DEBUG: paintEvent.*firstLine=(\d+) lastLine=(\d+) /) {
+    if (/(\d\d\.\d\d\d) DEBUG4.*paintEvent.*firstLine=(\d+) lastLine=(\d+) /) {
         if ( ($3 - $2) > 35 ) {
             $beginning = $1;
             $first_line = $2;
         }
     }
-    elsif (/(\d\d\.\d\d\d) DEBUG: End/) {
+    elsif (/(\d\d\.\d\d\d) DEBUG4.*End/) {
         if ($beginning) {
             $time = $1 - $beginning;
             # print "$first_line $time\n";

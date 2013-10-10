6c22ec0ebc97f8eb2f918fb5addec2448a8cef1b
Nicolas Bonnefon <nbonnefo@cisco.com>
2013-10-10 14:48:21 +0100

    Fix the release script to include the version file in the tarball.

--- release-source.sh
+++ release-source.sh
@@ -3,8 +3,10 @@
 VERSION=$(git describe | sed -e "s/^v//")
 echo "echo \"$VERSION\" > .tarball-version"
 echo 'touch --date="$(git log -n 1 --pretty=format:%ci)" .tarball-version'
-echo "git archive --format=tar --prefix=glogg-$VERSION/ v$VERSION >glogg-$VERSION.tmp"
-echo "tar --append -f glogg-$VERSION.tmp --transform s_^_glogg-$VERSION/_ .tarball-version"
-echo "gzip -n < glogg-$VERSION.tmp > glogg-$VERSION.tar.gz"
-echo "rm .tarball-version glogg-$VERSION.tmp"
-
+echo "tmp_tar=\"$(mktemp)\""
+echo "git archive --format=tar --prefix=glogg-$VERSION/ v$VERSION >\$tmp_tar"
+echo "tmp_dir=\"$(mktemp -d)\""
+echo "tar xf \$tmp_tar --directory \$tmp_dir"
+echo "cp -p .tarball-version \$tmp_dir/glogg-$VERSION/"
+echo "tar czf glogg-$VERSION.tar.gz --directory \$tmp_dir glogg-$VERSION/"
+echo "rm .tarball-version \$tmp_tar"

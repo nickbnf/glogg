7fc76d9de353692bfd946e82a473c5ce1cc407ec
Paolo Cancedda <paolo.cancedda@gmail.com>
2015-09-03 20:46:28 +0200

    Emit newSelection() signal when changing selection with the keyboard

--- src/abstractlogview.h
+++ src/abstractlogview.h
@@ -374,6 +374,7 @@ class AbstractLogView :
     int convertCoordToColumn( int xPos ) const;
     void displayLine( LineNumber line );
     void moveSelection( int y );
+    void moveSelectionAndEmit(int delta);
     void moveSelectionUp();
     void moveSelectionDown();
     void jumpToStartOfLine();

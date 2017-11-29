5d8883b1f8bfbe884e23f9cfa1b13ee99665ed63
Anton Filimonov <anton.filimonov@gmail.com>
2017-11-29 08:54:54 +0300

    Use cpack to create DMG from macdeployqt bundle

---
+++ scripts/osx/install_deps.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+brew update
+brew install cmake
+brew install qt5
+brew link qt5 --force
--- scripts/osx/prepare_bundle.sh
+++ scripts/osx/prepare_bundle.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 macdeployqt ./output/klogg.app -always-overwrite -verbose=2
 python ../3rdparty/macdeployqtfix/macdeployqtfix.py ./output/klogg.app/Contents/MacOS/klogg $(brew --prefix qt5)
-macdeployqt ./output/klogg.app -dmg -no-plugins

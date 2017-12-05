8b8fff7866bf4687de3f4ec03acb8bb6388c0048
Anton Filimonov <anton.filimonov@gmail.com>
2017-12-05 23:23:30 +0300

    Switch back to ninja on travis

--- scripts/linux/make_appimage.sh
+++ scripts/linux/make_appimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-make INSTALL_ROOT=appdir install
+DESTDIR=$(readlink -f appdir) ninja install
 wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmake=/opt/qt59/bin/qmake -bundle-non-qt-libs
--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 brew update
 brew install cmake
+brew install ninja
 brew install qt5
 brew link qt5 --force

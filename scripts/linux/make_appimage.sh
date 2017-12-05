2f1e8b67e445b6ecf053fcb0640e0f79f78528f0
Anton Filimonov <anton.filimonov@gmail.com>
2017-12-05 23:00:23 +0300

    Don't use ninja on travis

--- scripts/linux/make_appimage.sh
+++ scripts/linux/make_appimage.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-DESTDIR=$(readlink -f appdir) ninja install
+make INSTALL_ROOT=appdir install
 wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
 chmod a+x linuxdeployqt-continuous-x86_64.AppImage
 ./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmake=/opt/qt59/bin/qmake -bundle-non-qt-libs

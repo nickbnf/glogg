9f850936f193b6e0057829e50d76cbf76e3a62c7
Nicolas Bonnefon <nicolas@bonnefon.org>
2018-08-05 18:53:04 +0100

    Ensure the file is re-opened when it is deleted and recreated.
    Fix #151

--- src/data/logdata.cpp
+++ src/data/logdata.cpp
@@ -197,25 +197,46 @@ void LogData::startOperation()
 
 void LogData::fileChangedOnDisk()
 {
-    LOG(logDEBUG) << "signalFileChanged";
-
     const QString name = attached_file_->fileName();
-    QFileInfo info( name );
 
-    // Need to open the file in case it was absent
-    attached_file_->open( QIODevice::ReadOnly );
+    LOG(logDEBUG) << "signalFileChanged: " << name.toStdString();
+
+    QFileInfo info( name );
+    qint64 file_size = indexing_data_.getSize();
+    LOG(logDEBUG) << "current indexed fileSize=" << file_size;
+    LOG(logDEBUG) << "info file_->size()=" << info.size();
+    LOG(logDEBUG) << "attached_file_->size()=" << attached_file_->size();
+    // In absence of any clearer information, we use the following size comparison
+    // to determine whether we are following the same file or not (i.e. the file
+    // has been moved and the inode we are following is now under a new name, if for
+    // instance log has been rotated). We want to follow the name so we have to reopen
+    // the file to ensure we are reading the right one.
+    // This is a crude heuristic but necessary for notification services that do not
+    // give details (e.g. kqueues)
+    if ( ( info.size() != attached_file_->size() )
+            || ( attached_file_->openMode() == QIODevice::NotOpen ) ) {
+        LOG(logINFO) << "Inconsistent size, the file might have changed, re-opening";
+        auto reopened = std::make_unique<QFile>( name );
+        reopened->open( QIODevice::ReadOnly );
+        QMutexLocker locker( &fileMutex_ );
+        attached_file_ = std::move( reopened );      // This will close the old one and open the new
+
+        // We don't force a (slow) full reindex as this routinely happens if
+        // the file is appended quickly.
+        // This means we can occasionally have false negatives (should be dealt with at
+        // a lower level): e.g. if a new file is created with the same name as the old one
+        // and with a size greater than the old one (should be rare in practice).
+    }
 
     std::shared_ptr<LogDataOperation> newOperation;
 
-    qint64 file_size = indexing_data_.getSize();
-    LOG(logDEBUG) << "current fileSize=" << file_size;
-    LOG(logDEBUG) << "info file_->size()=" << info.size();
-    if ( info.size() < file_size ) {
+    qint64 real_file_size = attached_file_->size();
+    if ( real_file_size < file_size ) {
         fileChangedOnDisk_ = Truncated;
         LOG(logINFO) << "File truncated";
         newOperation = std::make_shared<FullIndexOperation>();
     }
-    else if ( info.size() == file_size ) {
+    else if ( real_file_size == file_size ) {
         LOG(logINFO) << "No change in file";
     }
     else if ( fileChangedOnDisk_ != DataAdded ) {

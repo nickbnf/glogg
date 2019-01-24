82b3d16a94875f1d2bef55dfd0777eb40e219c10
Anton Filimonov <anton.filimonov@gmail.com>
2019-01-25 02:03:05 +0300

    revert to manual deps install

--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-#brew update
-#brew install cmake
-#brew install ninja
-#brew install pandoc
-#brew install qt
+brew update
+brew install cmake
+brew install ninja
+brew install pandoc
+brew install qt
 brew link qt5 --force

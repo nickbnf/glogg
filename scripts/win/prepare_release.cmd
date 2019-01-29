926d168dc66056aa8e382e52c3388d7e69970b48
Anton Filimonov <anton.filimonov@gmail.com>
2019-01-29 13:18:58 +0300

    copy custom tbbmalloc to release dir

--- scripts/win/prepare_release.cmd
+++ scripts/win/prepare_release.cmd
@@ -6,8 +6,8 @@ xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg.exe %APPVEYOR_BUILD_FOLDER%\rel
 xcopy "%VCToolsRedistDir%x64\Microsoft.VC141.CRT\msvcp140.dll" %APPVEYOR_BUILD_FOLDER%\release\ /y
 xcopy "%VCToolsRedistDir%x64\Microsoft.VC141.CRT\vcruntime140.dll" %APPVEYOR_BUILD_FOLDER%\release\ /y
 
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\tbbmalloc.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\tbbmalloc_proxy.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc_proxy.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
 
 xcopy %QTDIR%\bin\Qt5Core.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
 xcopy %QTDIR%\bin\Qt5Gui.dll %APPVEYOR_BUILD_FOLDER%\release\ /y

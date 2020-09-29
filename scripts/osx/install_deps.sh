0d8b15ddb2d196bc3966a72894ae87d1f720741b
Anton Filimonov <anton.filimonov@gmail.com>
2020-09-29 20:55:41 +0300

    Try use qt 5.12 on osx

--- scripts/osx/install_deps.sh
+++ scripts/osx/install_deps.sh
@@ -1,7 +1,9 @@
-#!/bin/bash
-#brew update
-#brew install cmake
-#brew install ninja
-#brew install pandoc
-#brew install qt
-brew link qt5 --force
+#ninja
+wget -v https://github.com/ninja-build/ninja/releases/download/v1.10.1/ninja-mac.zip -O ninja-mac.zip
+unzip ninja-mac.zip
+
+#qt 5.12
+brew tap-new variar/qt
+brew extract --version 5.12.3 qt variar/qt
+brew install variar/qt/qt@5.12.3
+brew link qt --force

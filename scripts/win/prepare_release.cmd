fa61410c36e10c2fafe5494e8fb0195ffd89eaeb
Anton Filimonov <anton.filimonov@gmail.com>
2020-09-30 22:30:26 +0300

    Fix portable packaging

--- scripts/win/prepare_release.cmd
+++ scripts/win/prepare_release.cmd
@@ -48,5 +48,5 @@ xcopy %KLOGG_WORKSPACE%\packaging\windows\chocolatelyInstall.ps1 chocolately\too
 xcopy %KLOGG_WORKSPACE%\packaging\windows\klogg.nsi  /y
 xcopy %KLOGG_WORKSPACE%\packaging\windows\FileAssociation.nsh  /y
 
-7z a %KLOGG_WORKSPACE%\klogg-%KLOGG_VERSION%-%KLOGG_ARCH%-portable.7z @%KLOGG_WORKSPACE%\scripts\win\7z_klogg_listfile.txt
+7z a -r %KLOGG_WORKSPACE%\klogg-%KLOGG_VERSION%-%KLOGG_ARCH%-portable.7z @%KLOGG_WORKSPACE%\scripts\win\7z_klogg_listfile.txt
 7z a %KLOGG_WORKSPACE%\klogg-%KLOGG_VERSION%-%KLOGG_ARCH%-pdb.7z @%KLOGG_WORKSPACE%\scripts\win\7z_pdb_listfile.txt

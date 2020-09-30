076d16b155395998f6ba8958382c9c8bf9f16953
Anton Filimonov <anton.filimonov@gmail.com>
2020-09-30 10:46:51 +0300

    Try packaging in github workflow

--- scripts/win/prepare_release.cmd
+++ scripts/win/prepare_release.cmd
@@ -1,45 +1,52 @@
-md %APPVEYOR_BUILD_FOLDER%\release
+set "QTDIR=%Qt5_Dir%"
+set "Qt5_DIR=%QTDIR:/=\%"
+echo %Qt5_DIR%
 
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_portable.exe %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_portable.pdb %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg.exe %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg.pdb %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\generated\documentation.html %APPVEYOR_BUILD_FOLDER%\release\ /y
+md %KLOGG_WORKSPACE%\release
 
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc.pdb %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc_proxy.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\build\output\klogg_tbbmalloc_proxy.pdb %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_portable.exe %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_portable.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg.exe %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\generated\documentation.html %KLOGG_WORKSPACE%\release\ /y
 
-xcopy %APPVEYOR_BUILD_FOLDER%\COPYING %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\NOTICE %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\README.md %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\DOCUMENTATION.md %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_tbbmalloc.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_tbbmalloc.pdb %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_tbbmalloc_proxy.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\build\output\klogg_tbbmalloc_proxy.pdb %KLOGG_WORKSPACE%\release\ /y
 
-xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\msvcp140.dll" %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\vcruntime140.dll" %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\vcruntime140_1.dll" %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %KLOGG_WORKSPACE%\COPYING %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\NOTICE %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\README.md %KLOGG_WORKSPACE%\release\ /y
+xcopy %KLOGG_WORKSPACE%\DOCUMENTATION.md %KLOGG_WORKSPACE%\release\ /y
 
-xcopy %QTDIR%\bin\Qt5Core.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %QTDIR%\bin\Qt5Gui.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %QTDIR%\bin\Qt5Network.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %QTDIR%\bin\Qt5Widgets.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %QTDIR%\bin\Qt5Concurrent.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %QTDIR%\bin\Qt5Xml.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\msvcp140.dll" %KLOGG_WORKSPACE%\release\ /y
+xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\vcruntime140.dll" %KLOGG_WORKSPACE%\release\ /y
+xcopy "%VCToolsRedistDir%%platform%\Microsoft.VC142.CRT\vcruntime140_1.dll" %KLOGG_WORKSPACE%\release\ /y
 
-md %APPVEYOR_BUILD_FOLDER%\release\platforms
-xcopy %QTDIR%\plugins\platforms\qwindows.dll %APPVEYOR_BUILD_FOLDER%\release\platforms\ /y
-md %APPVEYOR_BUILD_FOLDER%\release\styles
-xcopy %QTDIR%\plugins\styles\qwindowsvistastyle.dll %APPVEYOR_BUILD_FOLDER%\release\styles /y
+md %KLOGG_WORKSPACE%\release\platforms
+md %KLOGG_WORKSPACE%\release\styles
 
-xcopy %SSL_DIR%\libcrypto-1_1%SSL_ARCH%.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
-xcopy %SSL_DIR%\libssl-1_1%SSL_ARCH%.dll %APPVEYOR_BUILD_FOLDER%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Core.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Gui.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Network.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Widgets.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Concurrent.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\bin\Qt5Xml.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %Qt5_DIR%\plugins\platforms\qwindows.dll %KLOGG_WORKSPACE%\release\platforms\ /y
+xcopy %Qt5_DIR%\plugins\styles\qwindowsvistastyle.dll %KLOGG_WORKSPACE%\release\styles /y
 
-md %APPVEYOR_BUILD_FOLDER%\chocolately
-md %APPVEYOR_BUILD_FOLDER%\chocolately\tools
+xcopy %SSL_DIR%\libcrypto-1_1%SSL_ARCH%.dll %KLOGG_WORKSPACE%\release\ /y
+xcopy %SSL_DIR%\libssl-1_1%SSL_ARCH%.dll %KLOGG_WORKSPACE%\release\ /y
 
-xcopy %APPVEYOR_BUILD_FOLDER%\packaging\windows\klogg.nuspec %APPVEYOR_BUILD_FOLDER%\chocolately /y
-xcopy %APPVEYOR_BUILD_FOLDER%\packaging\windows\chocolatelyInstall.ps1 %APPVEYOR_BUILD_FOLDER%\chocolately\tools\ /y
+md %KLOGG_WORKSPACE%\chocolately
+md %KLOGG_WORKSPACE%\chocolately\tools
 
-xcopy %APPVEYOR_BUILD_FOLDER%\packaging\windows\klogg.nsi %APPVEYOR_BUILD_FOLDER%\ /y
-xcopy %APPVEYOR_BUILD_FOLDER%\packaging\windows\FileAssociation.nsh %APPVEYOR_BUILD_FOLDER%\ /y
+xcopy %KLOGG_WORKSPACE%\packaging\windows\klogg.nuspec chocolately /y
+xcopy %KLOGG_WORKSPACE%\packaging\windows\chocolatelyInstall.ps1 chocolately\tools\ /y
+
+xcopy %KLOGG_WORKSPACE%\packaging\windows\klogg.nsi  /y
+xcopy %KLOGG_WORKSPACE%\packaging\windows\FileAssociation.nsh  /y
+
+7z a %KLOGG_WORKSPACE%\klogg-%KLOGG_VERSION%-%KLOGG_ARCH%-portable.7z @%KLOGG_WORKSPACE%\scripts\win\7z_klogg_listfile.txt
+7z a %KLOGG_WORKSPACE%\klogg-%KLOGG_VERSION%-%KLOGG_ARCH%-pdb.7z @%KLOGG_WORKSPACE%\scripts\win\7z_pdb_listfile.txt

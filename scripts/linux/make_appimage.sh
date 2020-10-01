b6d27131f630963b9d4380230e00f305dc970a3b
Anton Filimonov <anton.filimonov@gmail.com>
2020-10-01 08:55:24 +0300

    [skip ci] Try fix additional ci builds

--- scripts/linux/make_appimage.sh
+++ scripts/linux/make_appimage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DESTDIR=$(readlink -f appdir) ninja install
-wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/continuous/linuxdeployqt-continuous-x86_64.AppImage"
-chmod a+x linuxdeployqt-continuous-x86_64.AppImage
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmake=/opt/qt59/bin/qmake -bundle-non-qt-libs
-./linuxdeployqt-continuous-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage
+wget -c -q "https://github.com/probonopd/linuxdeployqt/releases/download/7/linuxdeployqt-7-x86_64.AppImage"
+chmod a+x linuxdeployqt-7-x86_64.AppImage
+./linuxdeployqt-7-x86_64.AppImage appdir/usr/share/applications/*.desktop -qmake=$Qt5_Dir/bin/qmake -bundle-non-qt-libs
+./linuxdeployqt-7-x86_64.AppImage appdir/usr/share/applications/*.desktop -appimage

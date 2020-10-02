de7fae009affe2a4405c8b0530d47bcc7666ca60
Anton Filimonov <anton.filimonov@gmail.com>
2020-10-02 23:56:34 +0300

    Try to codesign dmg (#217)

--- scripts/osx/prepare_bundle.sh
+++ scripts/osx/prepare_bundle.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+codesign -v -f -o runtime --timestamp -s "Developer ID Application: Anton Filimonov (GAW773U324)" ./output/klogg.app/Contents/MacOS/crash_handler
+codesign -v -f -o runtime --timestamp -s "Developer ID Application: Anton Filimonov (GAW773U324)" ./output/klogg.app/Contents/MacOS/minidump_stackwalk
+
 macdeployqt ./output/klogg.app -always-overwrite -verbose=2
+
 python ../3rdparty/macdeployqtfix/macdeployqtfix.py ./output/klogg.app/Contents/MacOS/klogg $(brew --prefix qt5)
+
+codesign -v -f -o runtime --deep --timestamp -s "Developer ID Application: Anton Filimonov (GAW773U324)" ./output/klogg.app;

3b8a581e7aa73657af219a167862da384b8cfac3
Anton Filimonov <anton.filimonov@gmail.com>
2021-04-12 00:36:53 +0300

    Reduce allocation on indexing

--- src/logdata/src/logdataworker.cpp
+++ src/logdata/src/logdataworker.cpp
@@ -195,7 +195,7 @@ void LogDataWorker::indexAll( QTextCodec* forcedEncoding )
     operationFuture_.waitForFinished();
     interruptRequest_.clear();
 
-    operationFuture_ = QtConcurrent::run( [this, forcedEncoding, fileName = fileName_] {
+    operationFuture_ = QtConcurrent::run( [ this, forcedEncoding, fileName = fileName_ ] {
         auto operationRequested = std::make_unique<FullIndexOperation>(
             fileName, indexing_data_, interruptRequest_, forcedEncoding );
         return connectSignalsAndRun( operationRequested.get() );
@@ -210,7 +210,7 @@ void LogDataWorker::indexAdditionalLines()
     operationFuture_.waitForFinished();
     interruptRequest_.clear();
 
-    operationFuture_ = QtConcurrent::run( [this, fileName = fileName_] {
+    operationFuture_ = QtConcurrent::run( [ this, fileName = fileName_ ] {
         auto operationRequested = std::make_unique<PartialIndexOperation>( fileName, indexing_data_,
                                                                            interruptRequest_ );
         return connectSignalsAndRun( operationRequested.get() );
@@ -225,7 +225,7 @@ void LogDataWorker::checkFileChanges()
     operationFuture_.waitForFinished();
     interruptRequest_.clear();
 
-    operationFuture_ = QtConcurrent::run( [this, fileName = fileName_] {
+    operationFuture_ = QtConcurrent::run( [ this, fileName = fileName_ ] {
         auto operationRequested = std::make_unique<CheckFileChangesOperation>(
             fileName, indexing_data_, interruptRequest_ );
 
@@ -288,11 +288,11 @@ FastLinePositionArray IndexOperation::parseDataBlock( LineOffset::UnderlyingType
 
     int pos_within_block = 0;
 
-    const auto adjustToCharWidth = [&state]( auto pos ) {
+    const auto adjustToCharWidth = [ &state ]( auto pos ) {
         return static_cast<int>( pos ) - state.encodingParams.getBeforeCrOffset();
     };
 
-    const auto expandTabs = [&]( const char* start_in_line, auto line_size ) {
+    const auto expandTabs = [ & ]( const char* start_in_line, auto line_size ) {
         auto tab_search_start = start_in_line;
         auto next_tab = reinterpret_cast<const char*>(
             std::memchr( tab_search_start, '\t', static_cast<size_t>( line_size ) ) );
@@ -439,29 +439,33 @@ void IndexOperation::doIndex( LineOffset initialPosition )
 
     auto blockReaderAsync = tbb::flow::async_node<tbb::flow::continue_msg, BlockData>(
         indexingGraph, tbb::flow::serial,
-        [this, &localThreadPool, &file, &ioDuration]( const auto&, auto& gateway ) {
+        [ this, &localThreadPool, &file, &ioDuration ]( const auto&, auto& gateway ) {
             gateway.reserve_wait();
 
             QtConcurrent::run(
-                &localThreadPool, [this, &file, &ioDuration, gw = std::ref( gateway )] {
+                &localThreadPool, [ this, &file, &ioDuration, gw = std::ref( gateway ) ] {
                     while ( !file.atEnd() ) {
 
                         if ( interruptRequest_ ) {
                             break;
                         }
-                        QByteArray readBuffer( IndexingBlockSize, Qt::Uninitialized );
 
-                        BlockData blockData;
-                        blockData.first = file.pos();
+                        BlockData blockData{ file.pos(),
+                                             QByteArray{ IndexingBlockSize, Qt::Uninitialized } };
+
                         clock::time_point ioT1 = clock::now();
-                        const auto readBytes = file.read( readBuffer.data(), readBuffer.size() );
+                        const auto readBytes
+                            = static_cast<int>( file.read( blockData.second.data(), blockData.second.size() ) );
 
                         if ( readBytes < 0 ) {
                             LOG_ERROR << "Reading past the end of file";
                             break;
                         }
 
-                        blockData.second = readBuffer.left( static_cast<int>( readBytes ) );
+                        if ( readBytes < blockData.second.size() ) {
+                            blockData.second.resize( readBytes );
+                        }
+
                         clock::time_point ioT2 = clock::now();
 
                         ioDuration += duration_cast<milliseconds>( ioT2 - ioT1 );
@@ -487,7 +491,7 @@ void IndexOperation::doIndex( LineOffset initialPosition )
     auto blockQueue = tbb::flow::queue_node<BlockData>( indexingGraph );
 
     auto blockParser = tbb::flow::function_node<BlockData, tbb::flow::continue_msg>(
-        indexingGraph, 1, [this, &state]( const BlockData& blockData ) {
+        indexingGraph, 1, [ this, &state ]( const BlockData& blockData ) {
             const auto& block_beginning = blockData.first;
             const auto& block = blockData.second;
 
@@ -674,7 +678,7 @@ MonitoredFileStatus CheckFileChangesOperation::doCheckFileChanges()
             return MonitoredFileStatus::Truncated;
         }
 
-        const auto getDigest = [&file, &buffer]( const qint64 indexedSize ) {
+        const auto getDigest = [ &file, &buffer ]( const qint64 indexedSize ) {
             FileDigest fileDigest;
             auto readSize = 0ll;
             auto totalSize = 0ll;

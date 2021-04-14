ec7455d1286cd51734886c3ce9adf51cbb8a2432
Anton Filimonov <anton.filimonov@gmail.com>
2021-04-14 10:11:18 +0300

    [skip ci] Switch choco links to github release

--- packaging/windows/chocolatey/tools/chocolateyInstall.ps1
+++ packaging/windows/chocolatey/tools/chocolateyInstall.ps1
@@ -4,10 +4,10 @@ $packageArgs = @{
   packageName    = 'klogg'
   unzipLocation  = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
   fileType       = 'exe'
-  url64bit       = 'https://bintray.com/variar/generic/download_file?file_path=win%2Fklogg%2Fklogg-20.12.0.813-x64-setup.exe'
+  url64bit       = 'https://github.com/variar/klogg/releases/download/v20.12/klogg-20.12.0.813-x64-setup.exe'
   checksum64     = 'b51762fa09c9646172dd9efcfb985dad9376548deda6e513dc08f0a558e075ca'
   checksumType64 = 'sha256'
-  url            = 'https://bintray.com/variar/generic/download_file?file_path=win%2Fklogg%2Fklogg-20.12.0.813-x86-setup.exe'
+  url            = 'https://github.com/variar/klogg/releases/download/v20.12/klogg-20.12.0.813-x86-setup.exe'
   checksum       = 'f98d57e9a03e34935c3f8465b6244d7ec740e8cd498cbdb06577eab693a2bae1'
   checksumType   = 'sha256'
   softwareName   = 'klogg'
